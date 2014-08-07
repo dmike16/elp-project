28d218464c9f779d2c9208addbc1bbb841123b05
dmike16 <cipmiky@gmail.com>
2014-08-07 17:29:41 +0200

    Chapter two esercize number one

---
+++ ch2/es1/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 07/ago/2014
+ *      Author: dmike
+ */
+
+#include <iostream>
+//using namespace std;
+
+int main ()
+{	std::cout << "Hello Android World"
+	<< std::endl << "Going to Modd your rom"<<std::endl;
+
+	return 0;
+}
