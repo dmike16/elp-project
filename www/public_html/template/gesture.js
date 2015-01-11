5056fd5b276c938f5565b100429abec202f8dfc3
dmike16 <cipmiky@gmail.com>
2015-01-11 23:27:26 +0100

    Athor javaScript learnig

--- www/public_html/template/gesture.js
+++ www/public_html/template/gesture.js
@@ -1,11 +1,33 @@
 /* JavaScript Learning*/
 
-function sayHi(){
-    alert("Bella zio");
-}
-function callAnotherFunction(fnFunction,vArgument){
-    fnFunction(vArgument);
+function StringBuffer (){
+	this.__strings__=new Array;
+	
+	if(typeof StringBuffer._initialized == "undefined"){
+		StringBuffer.prototype.append = function (str){	
+			this.__strings__.push(str);
+		};
+		StringBuffer.prototype.toString = function(){
+			return this.__strings__.join("");
+		};
+	}
+	StringBuffer._initialized = true;
 }
 
-callAnotherFunction(sayHi);
-alert(callAnotherFunction.toString());
+var d1 = new Date();
+var str="";
+for(var i=0; i< 100; i++){
+	str += "text";
+}
+var d2 = new Date();
+
+alert("Concatenation with +: "+ (d2.getTime()-d1.getTime()));
+
+var oBuffer = new StringBuffer();
+d1 = new Date();
+for (var i=0; i < 100; i++) {
+	oBuffer.append("text");
+}
+var sResult = oBuffer.toString();
+d2 = new Date();
+alert("Concatenation with StringBuffer: " + (d2.getTime() - d1.getTime()));
