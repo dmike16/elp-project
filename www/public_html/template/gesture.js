763c4e20d411b0e3c954b441fdc5d9d8904fd0e1
dmike16 <cipmiky@gmail.com>
2015-02-05 00:20:28 +0100

    Inheritance

--- www/public_html/template/gesture.js
+++ www/public_html/template/gesture.js
@@ -1,33 +1,43 @@
 /* JavaScript Learning*/
-
-function StringBuffer (){
-	this.__strings__=new Array;
-	
-	if(typeof StringBuffer._initialized == "undefined"){
-		StringBuffer.prototype.append = function (str){	
-			this.__strings__.push(str);
-		};
-		StringBuffer.prototype.toString = function(){
-			return this.__strings__.join("");
-		};
-	}
-	StringBuffer._initialized = true;
+function Polygon(iSides){
+    this.sides=iSides;
+    if(typeof Polygon._initialized == "undefined"){
+        Polygon.prototype.getArea=function(){
+            return 0;
+        };
+    }
+    Polygon._initialized = true;
 }
 
-var d1 = new Date();
-var str="";
-for(var i=0; i< 100; i++){
-	str += "text";
+function Triangle(iBase,iHeight){
+    Polygon.call(this,3);
+    this.base = iBase;
+    this.height = iHeight;
+    if(typeof Triangle._initialized == "undefined"){
+        Triangle.prototype.getArea = function(){
+            return 0.5*this.base*this.height;
+        };
+    }
+    Triangle._initialized = true;
 }
-var d2 = new Date();
+Triangle.prototype = Object.create(Polygon);
 
-alert("Concatenation with +: "+ (d2.getTime()-d1.getTime()));
-
-var oBuffer = new StringBuffer();
-d1 = new Date();
-for (var i=0; i < 100; i++) {
-	oBuffer.append("text");
+function Rectangle(iLenght,iWidth){
+    Polygon.call(this,4);
+    this.lenght = iLenght;
+    this.width = iWidth;
+    if(typeof Rectangle._initialized == "undefined"){
+        Rectangle.prototype.getArea =function(){
+            return this.lenght*this.width;
+        };
+    }
 }
-var sResult = oBuffer.toString();
-d2 = new Date();
-alert("Concatenation with StringBuffer: " + (d2.getTime() - d1.getTime()));
+Rectangle.prototype = new Polygon();
+
+var tri = new Triangle(12,4);
+var rec = new Rectangle(22,11);
+alert(tri.sides);
+alert(tri.getArea());
+
+alert(rec.sides);
+alert(rec.getArea());
