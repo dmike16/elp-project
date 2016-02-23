5182b2371d4b29bcc99032e52bb78ac027dbae15
dmike16 <cipmiky@gmail.com>
2016-02-23 17:22:50 +0100

    Example of Ajax

--- WebAKT/ecommerce/js/game.js
+++ WebAKT/ecommerce/js/game.js
@@ -5,8 +5,23 @@ $(function($){
 	};
 	
 	$("button").on("mouseover",{lim: bound},function(e){
-		var x = e.data.width*Math.random();
-		var y = e.data.height*Math.random();
+		var lambda = Math.random();
+		var lim = e.data.lim;
+		var x = (e.clientX - (lim.width*lambda));
 		
+		if((e.clientX + x) >= lim.width || (e.clientX +x) <= 0){
+			x = e.clientX - lim.width /2;
+		}
+
+		lambda = Math.random();
+		var y = (e.clientY - (lim.heigth*lambda));
+		if((e.clientY + y) >= lim.heigth ||(e.clientY +y) <= 0){
+			y = e.clientY - lim.heigth / 2;
+		}
+		var style = {
+			transform: "translate("+x+"px,"+y+"px)"
+		};
+		
+		$(this).css(style);
 	});
 });
--- WebAKT/ecommerce/js/main.js
+++ WebAKT/ecommerce/js/main.js
@@ -1,16 +1,130 @@
-//Inserisce la scritta
-/*console.log("%cEcommerce","font-size:1.3em;color:blue;");
-var description = "Home Page";
-console.log("%c"+description,"font-size:1.3em;color:blue;");
+// Implementin and server asyncr call with jQuery
+(function() {
+    "use strict";
+    jQuery(document).ready(function($) {
 
-var height = window.innerHeight;
-console.log(height);
-// JQuery
-var title = $('title').text();
-console.log(title);
-//$('.container').append('<h1>brllas</h1>');
-$('<h1 class="titolo"><span>titolo</span></h1>').prependTo('.container');
-var container = $('.titolo').html();
-console.log(container);
-var row=$('.row');
-console.log(row);*/
+        var magazzino = "magazzino.xml";
+        $.ajax({
+            type: "GET",
+            url: magazzino,
+            dataType: "xml",
+            success: function(xml) {
+                $(xml).find("prodotto").each(function() {
+
+                    // Data fetch from the server
+                    var $xml = $(this);
+                    var sku = $xml.attr("sku");
+                    var $descr = $xml.find("descrizione");
+                    var descr = $descr.text();
+                    var nome = $descr.attr("nome");
+                    var imageSrc = $xml.find("imagine").text();
+                    var $prezzo = $xml.find("prezzo");
+                    var prezzo = parseFloat($prezzo.text());
+                    var sconto = parseInt($prezzo.attr('sconto'));
+
+                    //New Html Element
+                    var html = ['<div class="col-xs-12 col-sm-6 col-md-4">',
+                        '<section id="sku-'+sku+'" class="item" itemscope itemtype="http://schema.org/Product">',
+                        '<div class="image center" itemprop="image">',
+                        '<img src="' + imageSrc + '" alt="Chitarra Classica"/>',
+                        '</div>',
+                        '<h2 itemprop="name">' + nome + '</h2>',
+                        '<p itemprop="description">' + descr + '</p>',
+                        '<p itemprop="offer" itemscope itemtype="http://schema.org/Offer">Prezzo <span itemprop="price" class="strike">' + prezzo,
+                        '</span><span itemprop="priceCurrency" content="EUR">€</span> - ' + sconto + '% : <strong>' + (prezzo - (prezzo * sconto / 100)) + '€</strong></p>',
+                        '<label for="qty-1">',
+                        '<input itemprop="weight" id="qty-1" name="quantita" type="text" placeholder="Quatità" />',
+                        '</label>',
+                        '<button class="btn-rb add-to-cart" type="submit">Acquista</button>',
+                        '</section></div>'
+                    ];
+                    $("#central-row").append(html.join(" "));
+                });
+                $('.add-to-cart').on('click', function() {
+                	$('html').addClass('modal-open');
+                	var id = $(this).parent('.item').attr('id');
+                	var $container = $('#'+id);
+                   	var srcImage = $container.find('img').attr("src");
+                   	var nome = $container.find('h2[itemprop="name"]').text();
+                   	var price = $container.find('p[itemprop="offer"] strong').text();
+                   	var $modal = $('#myModal');
+                   	var $modal_header = $modal.find('h4');
+                   	$modal_header.text('Carrello');
+                   	var $modal_body = $modal.find('.modal-body');
+                   	$modal_body.empty();
+                   	var html = ['<h4>',nome,'</h4>','<img src="',srcImage,
+                   	'" style="width:120px;height:200px"/>',
+                   	'<p>Prezzo: ',
+                   	price,'</p>'];
+                   	$modal_body.append(html.join(""));
+                   	$modal.modal({
+                   		backdrop:'static'
+                   	});
+                   	console.log(srcImage,nome);
+                });
+                $('button[data-dismiss="modal"]').on('click',function(){
+                	$('html').removeClass('modal-open');
+                });
+
+            },
+            error: function() {
+                console.error("Somthing when wrong");
+            }
+        });
+
+
+    });
+}());
+
+// Cokies examples
+(function(doc){
+	//The cokies are in one document prop of the BOM
+	// cookies ora couple of value=anothervalue
+	var cookies = {
+		set: function setCookie(name,value,expire,ppath,ddomain,secure){
+			var cookie = name + '=' + encodeURIComponent(value);
+
+			if(expire){
+				cookie += '; expires=' + expire.toUTCString();
+			} 
+			if (ppath) {
+				cookie += '; path='+ppath;
+			}
+			if(ddomain){
+				cookie += '; domain='+ ddomain;
+			}
+			if(secure){
+				cookie += '; secure';
+			}
+			doc.cookie = cookie;
+		},
+		get: function getCookie(name){
+			var s = "(?:;\\s*)?" + name + "=([^;]*);?";
+			var reg = new RegExp(s);
+
+			if(reg.test(doc.cookie)){
+				return decodeURIComponent(RegExp.$1);
+			}else{
+				return null;
+			}
+		},
+		cancel: function cancelCookie(name,pth,dmn){
+			this.set(name,"",new Date(0),pth,dmn);
+		},
+		cancelAll: function cancelAll(){
+			var cookie = doc.cookie;
+			var reg = /(?:;\s*)?([^(?:path)(?:domain)(?:expires)(?:secure)(=)])/;
+			reg.test(cookie);
+			console.log(RegExp.$1);
+		}
+	};
+
+	cookies.set("name","Michele Cipolla");
+	cookies.set("message","Hello World",new Date(Date.parse('Jan 1, 2017')));
+	console.log(doc.cookie);
+	console.log(cookies.get('name'));
+	cookies.cancel('message');
+	console.log(doc.cookie);
+	cookies.cancelAll();
+	console.log(doc.cookie);
+}(document));
