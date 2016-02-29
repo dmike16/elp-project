e9e3a60a7e0e0403a4ea47bd5fa45f5a7b8e48b9
dmike16 <cipmiky@gmail.com>
2016-02-29 20:14:47 +0100

    Try to request the Google Maps API with CORS request but did not succide

--- WebAKT/ecommerce/js/injection.js
+++ WebAKT/ecommerce/js/injection.js
@@ -18,3 +18,31 @@ function loadScript(url, callback) {
     script.src = url;
     document.getElementsByTagName('head')[0].appendChild(script);
 }
+
+function corsScriptRequest(methdod, url) {
+    var xhr = new XMLHttpRequest();
+    if ('withCredentials' in xhr) {
+        xhr.open(methdod, url, true);
+    } else if (typeof XDomainRequest !== 'undefined') {
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+        throw new Error('No XMLHttpRequest support');
+    }
+    xhr.onreadyStatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.text = xhr.responseText;
+                document.body.appendChild(script);
+            }
+        }
+    };
+    xhr.onerror = function() {
+        console.log('Error in the request');
+    }
+
+    xhr.send();
+}
