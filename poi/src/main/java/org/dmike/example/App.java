a44c0313688e33c5269d3d2d0d8c9b880d053c23
dmike16 <cipmiky@gmail.com>
2016-11-13 21:01:43 +0100

    [3/4] example of general utils methood è image border ....

--- poi/src/main/java/org/dmike/example/App.java
+++ poi/src/main/java/org/dmike/example/App.java
@@ -7,23 +7,45 @@ import java.util.Calendar;
 import java.util.Date;
 
 import org.apache.poi.hssf.extractor.ExcelExtractor;
+import org.apache.poi.hssf.usermodel.DVConstraint;
+import org.apache.poi.hssf.usermodel.HSSFClientAnchor;
+import org.apache.poi.hssf.usermodel.HSSFDataValidation;
+import org.apache.poi.hssf.usermodel.HSSFPatriarch;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
+import org.apache.poi.hssf.usermodel.HSSFSimpleShape;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.ClientAnchor;
+import org.apache.poi.ss.usermodel.Comment;
 import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.DataValidation;
+import org.apache.poi.ss.usermodel.Drawing;
 import org.apache.poi.ss.usermodel.Header;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.PrintSetup;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.ShapeTypes;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.CellRangeAddressList;
+import org.apache.poi.ss.util.RegionUtil;
 import org.apache.poi.ss.util.WorkbookUtil;
 import org.apache.poi.xssf.extractor.XSSFExcelExtractor;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
+import org.apache.poi.xssf.usermodel.XSSFDataValidation;
+import org.apache.poi.xssf.usermodel.XSSFDataValidationConstraint;
+import org.apache.poi.xssf.usermodel.XSSFDataValidationHelper;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFSimpleShape;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.xmlbeans.XmlException;
 
@@ -90,9 +112,15 @@ public class App {
 		String sheet_name1 = WorkbookUtil.createSafeSheetName("Foglio 1?");
 		String sheet_name2 = WorkbookUtil.createSafeSheetName("Foglio 2");
 		String sheet_name3 = WorkbookUtil.createSafeSheetName("Foglio 3");
+		String sheet_name4 = WorkbookUtil.createSafeSheetName("Shape");
+		String sheet_name5 = WorkbookUtil.createSafeSheetName("validation");
+		
 		Sheet sheet1 = w.createSheet(sheet_name1);
 		Sheet sheet2 = w.createSheet(sheet_name2);
 		Sheet sheet3 = w.createSheet(sheet_name3);
+		Sheet sheet4 = w.createSheet(sheet_name4);
+		Sheet sheet5 = w.createSheet(sheet_name5);
+		
 		// Foglio Uno
 		w.setPrintArea(0, "$A$1:$C$2");
 		sheet1.setZoom(75);
@@ -103,8 +131,8 @@ public class App {
 		CellStyle cs = w.createCellStyle();
 		Row row = sheet1.createRow(0);
 		Cell cell = row.createCell(0);
-		cell.setCellValue(1);
-
+		cell.setCellValue("111111111");
+		sheet1.setColumnWidth(cell.getColumnIndex(),8*256);
 		row.createCell(1).setCellValue(1.2);
 		row.createCell(2).setCellValue(helper.createRichTextString("RichText"));
 		row.createCell(3).setCellValue(true);
@@ -121,9 +149,33 @@ public class App {
 		cell.setCellValue("Hello \n Work");
 		cs.setWrapText(true);
 		cell.setCellStyle(cs);
-		
 		row.setHeight((short) (sheet1.getDefaultRowHeight()*2));
 		sheet1.autoSizeColumn(6);
+		
+		row = sheet1.createRow(4);
+		cell = row.createCell(1);
+		cell.setCellValue("Test of merging");
+		CellRangeAddress region = CellRangeAddress.valueOf("B5:E6");
+		sheet1.addMergedRegion(region);
+		final short borderMediumDashed = BorderStyle.MEDIUM_DASHED.getCode();
+		RegionUtil.setBorderBottom(borderMediumDashed, region, sheet1);
+		RegionUtil.setBottomBorderColor(IndexedColors.AQUA.getIndex(), region, sheet1);
+		
+		row = sheet1.createRow(5);
+		cell = row.createCell(5);
+		cell.setCellValue("F4");
+		Drawing draw = sheet1.createDrawingPatriarch();
+		ClientAnchor ac = helper.createClientAnchor();
+		ac.setCol1(cell.getColumnIndex());
+		ac.setCol2(cell.getColumnIndex()+1);
+		ac.setRow1(row.getRowNum());
+		ac.setRow2(row.getRowNum()+3);
+		Comment comment = draw.createCellComment(ac);
+		comment.setString(helper.createRichTextString("Hello,boys"));
+		comment.setAuthor("dmike POI");
+		
+		cell.setCellComment(comment);
+		
 		// Foglio Due
 		Row row1 = sheet2.createRow(0);
 		Cell cell1 = row1.createCell(0);
@@ -148,12 +200,76 @@ public class App {
 				2,
 				1,
 				2));
+		sheet2.groupRow(5, 14);
+		sheet2.groupRow(7, 14);
+		sheet2.groupRow(16, 19);
+		
+		sheet2.groupColumn(4, 7);
+		sheet2.groupColumn(9, 12);
+		sheet2.groupColumn(10, 11);
 		//Foglio tre
 		Header header = sheet3.getHeader();
 		header.setCenter("Center");
 		header.setLeft("Left");
 		
 		sheet3.createSplitPane(2000, 2000, 0, 0, Sheet.PANE_LOWER_LEFT);
+		
+		Drawing patriarch = sheet4.createDrawingPatriarch();
+
+		if(w instanceof HSSFWorkbook){
+			//Foglio quattro
+			HSSFClientAnchor a = new HSSFClientAnchor(100,0,800,255,(short)1,0,(short)1,0);
+			HSSFPatriarch hsffpatriarch = (HSSFPatriarch)patriarch;
+			hsffpatriarch.createSimpleShape(a).
+			setShapeType(HSSFSimpleShape.OBJECT_TYPE_LINE);
+			a = new HSSFClientAnchor(0,0,1023,255,(short)1,1,(short)2,2);
+			hsffpatriarch.createTextbox(a).setString(new HSSFRichTextString("This a test"));
+			//Foglio cinque
+			DVConstraint constr = DVConstraint.createExplicitListConstraint(
+					new String[]{"10","20","30"});
+			CellRangeAddressList address = new CellRangeAddressList(0,0,0,0);
+			DataValidation data_v = new HSSFDataValidation(address,constr);
+			data_v.setSuppressDropDownArrow(true);
+			data_v.setErrorStyle(DataValidation.ErrorStyle.STOP);
+			data_v.createErrorBox("Error Box", "Attento");
+			data_v.createPromptBox("Prompt Box", "Message Text");
+			data_v.setShowPromptBox(true);
+			sheet5.addValidationData(data_v);
+		}else{
+			XSSFClientAnchor a = new XSSFClientAnchor();
+			a.setCol1(1);
+			a.setRow1(1);
+			a.setCol2(2);
+			a.setRow2(1);
+			a.setDx1(0);
+			a.setDy2(100000);
+			XSSFDrawing xssfpatriarch = (XSSFDrawing)patriarch;
+			XSSFSimpleShape shape = (xssfpatriarch).createSimpleShape(a);
+			shape.setShapeType(ShapeTypes.LINE);
+			shape.setLineWidth(1.5);
+			shape.setLineStyleColor(0,0,255);
+			a = new XSSFClientAnchor(100,0,1023,255,2,2,5,5);
+			XSSFSimpleShape text = xssfpatriarch.createTextbox(a);
+			text.setShapeType(ShapeTypes.RECT);
+			text.setFillColor(255, 255, 255);
+			text.setText("BElla");
+			
+			//Folgio cinque
+			XSSFDataValidationHelper dv_helper = new XSSFDataValidationHelper((XSSFSheet)sheet5);
+			XSSFDataValidationConstraint constr = (XSSFDataValidationConstraint)
+					dv_helper.createExplicitListConstraint(new String[]{"10","20","30"});
+			CellRangeAddressList address = new CellRangeAddressList(0,0,0,0);
+			XSSFDataValidation valide = (XSSFDataValidation)dv_helper.createValidation(constr, address);
+			//valide.setSuppressDropDownArrow(false);
+			valide.setShowErrorBox(true);
+			valide.setErrorStyle(DataValidation.ErrorStyle.STOP);
+			valide.createErrorBox("Error Box", "Text error");
+			valide.createPromptBox("Prompt Box", "Text message");
+			valide.setShowPromptBox(true);
+			sheet5.addValidationData(valide);
+			
+		}
+		
 	}
 
 	private static void writeToFile(String name, Workbook wk) throws IOException {
